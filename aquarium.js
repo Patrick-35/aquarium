b47a97c508a540f5b9c017729aaa25096ac3b97e
Patrick-35 <69558650+Patrick-35@users.noreply.github.com>
2020-09-16 11:17:22 +1000

    Update aquarium.js

--- aquarium.js
+++ aquarium.js
@@ -1,24 +1,31 @@
 window.onload=function() {
     lengthObj = document.getElementById('txtLength');
-    girthObj = document.getElementById('txtGirth');
-    weightObj = document.getElementById('tdWeight');
+    heightObj = document.getElementById('txtHeight');
+    widthObj = document.getElementById('txtWidth');
+	costObj = document.getElementById('tdCost');
     document.getElementById('btnReset').onclick = resetInputs;
-    document.getElementById('btnCalc').onclick = calcWeight;
+    document.getElementById('btnCalcCost').onclick = calcCost;
 }
 
 function resetInputs() {
     lengthObj.value = '';
-    girthObj.value = '';
-    weightObj.innerHTML = '';
+    heightObj.value = '';
+	widthObj.value = '';
+    costObj.innerHTML = '';
 }
 
-function calcWeight() {
+function calcCost() {
     var length = new Number(lengthObj.value);
-    var girth = new Number(girthObj.value);
-    weightObj.innerHTML = '';
-    if(isNaN(length) || isNaN(girth)) {
-		alert('Invalid length or girth');
+    var width = new Number(widthObj.value);
+	var height = new Number(heightObj.value);
+    costObj.innerHTML = '';
+    if(isNaN(length) || isNaN(height) || isNaN(width)) {
+		alert('Invalid length or height');
         return;
     }
-    weightObj.innerHTML = length*Math.pow(girth,2)/800;
+    var glasscost= (((length*width*2)+(width*height*2)+(length*height*2))*0.06);
+	var gluecost=((height*4)+(length*2)+(width*2))*0.1;
+	var labour= (60/6000)*((height*4)+(length*2)+(width*2));
+	var GST= (glasscost+gluecost+labour)*0.1;
+    costObj.innerHTML =glasscost+gluecost+labour+GST;
 }
